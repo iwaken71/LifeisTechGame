cf3727b02ce5dcce2063a885217a4954dbe09e37
iwaken71 <iwaken59371518@gmail.com>
2016-05-14 13:11:19 +0900

    a

---
+++ Assets/Test.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO;
+
+public class Test : MonoBehaviour {
+
+
+	public RenderTexture renderTextureRef;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetMouseButtonDown(0)){
+			Debug.Log (1);
+			savePng();
+
+		}
+	
+	}
+
+	void savePng(){
+
+		Texture2D tex = new Texture2D (renderTextureRef.width,renderTextureRef.height,TextureFormat.RGB24,false);
+		RenderTexture.active = renderTextureRef;
+		tex.ReadPixels(new Rect(0, 0, renderTextureRef.width, renderTextureRef.height), 0, 0);
+		tex.Apply();
+
+		// Encode texture into PNG
+		byte[] bytes = tex.EncodeToPNG();
+		Object.Destroy(tex);
+
+		//Write to a file in the project folder
+		File.WriteAllBytes(Application.dataPath + "/../SavedScreen.png", bytes);
+	}
+}
